3ed155a3b8269e730b5451fc1eb65a10eacdaa91
Bittu Shaw <bshaw891021@gmail.com>
2025-10-08 16:06:25 +0530

    changes

--- src/routes/AppRoutes.tsx
+++ src/routes/AppRoutes.tsx
@@ -2,7 +2,6 @@ import { Routes, Route } from "react-router-dom"
 import Home from "../pages/Home"
 import ScoreKeeper from "../pages/ScoreKeeper"
 import NotFound from "../pages/404"
-import ClockSynkFeedback from "../pages/FeedBackForm"
 
 // define type for settings
 interface Settings {
@@ -18,7 +17,6 @@ export default function AppRoutes({ settings }: AppRoutesProps) {
     <Routes>
       <Route path="/:fieldslug" element={<Home  settings={settings}/>} />
       <Route path="/score-keeper" element={<ScoreKeeper />} />
-      <Route path="/feedback" element={<ClockSynkFeedback />} />
       <Route path="*" element={<NotFound />} />
     </Routes>
   )
